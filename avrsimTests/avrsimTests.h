11d33aedd2ce480793a5bbff04cc4febb124f574
Chris Marrin <cmarrin@apple.com>
2011-07-01 10:23:43 -0700

    Initial Commit

---
+++ avrsimTests/avrsimTests.h
@@ -0,0 +1,13 @@
+//
+//  avrsimTests.h
+//  avrsimTests
+//
+//  Created by Chris Marrin on 7/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface avrsimTests : SenTestCase
+
+@end
---
+++ avrsimTests/avrsimTests.m
@@ -0,0 +1,32 @@
+//
+//  avrsimTests.m
+//  avrsimTests
+//
+//  Created by Chris Marrin on 7/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "avrsimTests.h"
+
+@implementation avrsimTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in avrsimTests");
+}
+
+@end
